88c2dcffadabc939ec2a21b4ef04c9a13c2f31a7
Vlad <vladd.rankov@yandex.by>
2024-06-01 14:11:31 +0300

    add corousel

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,24 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src=icons/arrow_left.svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src=icons/arrow_right.svg></button>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                arrows:false,
+                dots: true
+              }
+            }
+        ]
+    });
+  });
